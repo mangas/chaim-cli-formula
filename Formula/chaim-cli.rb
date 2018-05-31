e5a5c28e796b7a494bb171f5bf749218c3ec8067
Filipe Azevedo <filipe@azevedo.io>
2018-05-31 17:00:21 +0100

    version update

--- Formula/chaim-cli.rb
+++ Formula/chaim-cli.rb
@@ -2,7 +2,7 @@ class ChaimCli < Formula
   desc "Chaim command line interface"
   url "https://s3-eu-west-1.amazonaws.com/chaim-cli/chaim-latest.osx.tgz"
   version "1.5-0"
-  sha256 "0b297204382627f905bdea91f65e330ba724631a989efa05b49fe2928bb19c9d"
+  sha256 "6ab55c64bf9b88f448291f88732291003aa2cdd467ebe502abf2d2c7da39bf79"
 
   def install
 	prefix.install Dir["./*"]
