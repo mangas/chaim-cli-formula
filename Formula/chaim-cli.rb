b320d5a1bc11c71fde075455f963509723fc1eb4
Filipe Azevedo <filipe@azevedo.io>
2018-06-30 01:51:18 +0100

    update

--- Formula/chaim-cli.rb
+++ Formula/chaim-cli.rb
@@ -1,6 +1,6 @@
 class ChaimCli < Formula
   desc "Chaim command line interface"
-  url "https://s3-eu-west-1.amazonaws.com/chaim-cli/chaim-latest.osx.tgz"
+  url "https://s3-eu-west-1.amazonaws.com/chaim-cli/chaim-1.6-2.osx.tgz"
   version "1.6-2"
   sha256 "ad0a1bc109ccc6550090ab427dc7ae5d7289c22a29315b500d9577410f01730d"
 
